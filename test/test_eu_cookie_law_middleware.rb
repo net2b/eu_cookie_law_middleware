614c1a32619a5d9a03fee25dbdb5bccdf756fc1f
Elia Schito <elia@schito.me>
2015-06-01 17:32:11 +0200

    specs!

--- test/test_eu_cookie_law_middleware.rb
+++ test/test_eu_cookie_law_middleware.rb
@@ -1,18 +1,25 @@
 require 'minitest_helper'
 
 class TestEuCookieLawMiddleware < Minitest::Spec
-  def test_that_it_has_a_version_number
+  it 'has a version number' do
     refute_nil ::EuCookieLawMiddleware::VERSION
   end
 
-  def test_that_adds_the_banner
+  it 'adds the banner' do
     stack = EuCookieLawMiddleware.new(simple_app)
     assert_includes collect_body(stack.call({})), 'WE USE COOKIES'
   end
 
-  it "gets an error when trying to acces keys on the fake env" do
+  it "raises when trying to acces keys on the fake env" do
     template_proc = -> env { env['HTTP_HOST'] }
     assert_raises EuCookieLawMiddleware::FakeEnv do
+      EuCookieLawMiddleware.new(simple_app, template_proc: template_proc)
+    end
+  end
+
+  it "raises when passed a proc without env arg" do
+    template_proc = -> {  }
+    assert_raises ArgumentError do
       EuCookieLawMiddleware.new(simple_app, template_proc: template_proc)
     end
   end
